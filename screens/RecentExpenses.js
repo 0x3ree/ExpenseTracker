44ea1143c1b7b704ab9aa8ba15dda73c39430f97
Three <ottunade95@gmail.com>
2025-05-16 17:44:14 +0100

    Fix variable name in RecentExpenses to improve clarity in expense filtering

--- screens/RecentExpenses.js
+++ screens/RecentExpenses.js
@@ -6,10 +6,10 @@ import { getDateMinusdays } from "../util/Date";
 function RecentExpenses() {
   const expensesCtx = useContext(ExpensesContext);
   // in rder to get the recent expenses we are filtering the expenses array to get the expenses that are within the last 7 days
-  const recentExpenses = expensesCtx.expenses.filter((expenses) => {
+  const recentExpenses = expensesCtx.expenses.filter((expense) => {
     const today = new Date(); //this gets the current date
     const date7DaysAgo = getDateMinusdays(today, 7); // this gets the date 7 days ago
-    return expenses.date > date7DaysAgo;
+    return expense.date > date7DaysAgo;
   });
   return (
     <ExpensesOutput expenses={recentExpenses} expensesPeriod="Last 7 Days" />
